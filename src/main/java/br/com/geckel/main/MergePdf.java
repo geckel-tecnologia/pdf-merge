f782b365332f7985ec959dea8a9734492314e1ba
GIANCARLO ECKEL <geckel@gmail.com>
2020-09-29 17:45:14 -0300

    alterando pra receber lista de parâmetros dentro de arquivo

--- src/main/java/br/com/geckel/main/MergePdf.java
+++ src/main/java/br/com/geckel/main/MergePdf.java
@@ -1,13 +1,23 @@
 package br.com.geckel.main;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import br.com.geckel.lib.MergePDF;
 
 public class MergePdf {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		MergePDF merge = new MergePDF();
-		merge.JuncaoPDF(args);
+		String fullFileName = args[0];
+		try {
+			String[] files = new String(Files.readAllBytes(Paths.get(fullFileName))).trim().split(" ");
+			MergePDF merge = new MergePDF();
+			merge.JuncaoPDF(files);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
